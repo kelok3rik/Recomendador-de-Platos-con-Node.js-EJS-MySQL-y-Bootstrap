c8a5c8446a14d1c5ba2c6ae1152139cd1b7a70e2
Erik Cruz <74468810+kelok3rik@users.noreply.github.com>
2023-04-26 04:42:16 -0400

    FIN

---
+++ Controllers/crudPedido.js
@@ -0,0 +1,92 @@
+const conexion = require('../DATABASE/db');
+
+exports.save = (req, res) => {
+
+    const total_plato = req.body.totalPlato;
+    const id_cliente = req.body.idCliente;
+
+
+    const { detalles } = req.body;
+    console.log(req.body);
+
+    // Obtener la fecha actual
+    const today = new Date();
+    // Convertir la fecha a una cadena con el formato YYYY-MM-DD
+    const dateString = today.toISOString().slice(0, 10);
+    console.log(dateString);
+
+
+    const insertPedido = `INSERT INTO pedido (ID_Cliente,Fecha,Total) VALUES ('${id_cliente}','${dateString}','${total_plato}')`;
+    conexion.query(insertPedido, (err, result) => {
+        if (err) {
+            console.log('Error al insertar pedido:', err);
+            res.status(500).send('Error al insertar pedido en la base de datos.');
+            return;
+        }
+        const plato_id = result.insertId;
+        detalles.forEach(detalle => {
+            const { productoID, cantidad, total } = detalle;
+            const insertPedidoDetalleQuery = `INSERT INTO detalle_pedido (ID_Pedido, Plato_ID, Cantidad,Precio) VALUES (${plato_id}, ${productoID}, ${cantidad}, '${total}')`;
+            conexion.query(insertPedidoDetalleQuery, (err, result) => {
+                if (err) {
+                    console.log('Error al insertar  en detalle pedido:', err);
+                }
+            });
+        });
+        res.status(200).send('Plato registrado exitosamente.');
+        return;
+    });
+
+};
+
+exports.buscarRecomendacion = (req, res) => {
+    const id_cliente = req.params.ID;
+    new Promise((resolve, reject) => {
+      conexion.query('SELECT plato.ID FROM `plato` INNER JOIN detalle_pedido on plato.ID = detalle_pedido.Plato_ID INNER JOIN pedido on pedido.ID = detalle_pedido.ID_Pedido where pedido.ID_Cliente = ?;', [id_cliente], (err, rows) => {
+        if (err) {
+          console.log('Error al obtener recomendaciones:', err);
+          reject('Error al obtener recomendaciones.');
+        } else {
+          aux = '';
+          const rowsaux = [];
+          for (i = 0; i < rows.length; i++) {
+            if (rows[i].ID != aux) {
+              rowsaux.push(rows[i].ID);
+            }
+            aux = rows[i].ID;
+          }
+          resolve(rowsaux);
+        }
+      });
+    })
+      .then(rowsaux => {
+       
+        const promesas = rowsaux.map(element => {
+          return new Promise((resolve, reject) => {
+            conexion.query('CALL recomendacion(?)', [element], (err, rows) => {
+              if (err) {
+                console.log('Error al obtener recomendaciones:', err);
+                reject('Error al obtener recomendaciones.');
+              } else {
+                resolve(rows);
+              }
+            });
+          });
+        });
+  
+        Promise.all(promesas)
+          .then(data => {
+            
+            res.json(data);
+          })
+          .catch(error => {
+            
+            res.status(500).send(error);
+          });
+      })
+      .catch(error => {
+       
+        res.status(500).send(error);
+      });
+  }
+  
--- router.js
+++ router.js
@@ -102,7 +102,9 @@ router.get('/mantenimientoIngredientes', (req, res) => {
         if (error) {
             throw error;
         } else {
-            res.render('mantenimientoIngredientes', { results: results[0] });
+            console.log(results);
+            res.json(results);
+            // res.render('mantenimientoIngredientes', { results: results[0] });
         }
     })
 })
@@ -132,7 +134,7 @@ router.get('/editarIngredientes/:ID', (req, res) => {
 //PLATOS
 //RUTA PARA PLATOS
 router.get('/mantenimientoPlatos', (req, res) => {
-    conexion.query('CALL mostrarPlatos();', (error, results) => {
+    conexion.query('CALL mostrarPlatos()', (error, results) => {
         if (error) {
             throw error;
         } else {
@@ -173,55 +175,51 @@ router.get('/registrarPlato', (req, res) => {
 
 
 //RUTA PARA EDITAR PLATO
-router.get('/editarIngredientes/:ID', (req, res) => {
-    const id = req.params.ID;
-    conexion.query("SELECT * FROM ingrediente where ID=?", [id], (error, results) => {
-        if (error) {
-            throw error;
-        } else {
-            res.render('editarIngredientes', { results: results[0] });
-        }
-    })
-})
-
-
-
-
-    +
-    //PROCESOS ////////////////////////// AAAAAAAAAAAAAAAAAAA/////////////////////////
-    //RUTA PARA ordenar
-    router.get('/ordenar', (req, res) => {
-        res.render('ordenar');
-    })
-
-
-
-////// PUNTO PRUEBASSSSS ////// 
-
-// router.post('/registrarPlatoss', (req, res) => {
-
-//     const { plato_nombre, plato_precio, plato_descripcion, plato_categoria, ingredientes } = req.body;
-//     const insertPlatoQuery = `INSERT INTO plato (Nombre,Descripcion,Precio,ID_CATEOGIRA_PLATO) VALUES ('${plato_nombre},${plato_descripcion},${plato_precio},${plato_categoria}')`;
-//     conexion.query(insertPlatoQuery, (err, result) => {
-//         if (err) {
-//            // console.log('Error al insertar plato:', err);
-//            // res.status(500).send('Error al insertar plato en la base de datos.');
-//             return;
+// router.get('/editarIngredientes/:ID', (req, res) => {
+//     const id = req.params.ID;
+//     conexion.query("SELECT * FROM ingrediente where ID=?", [id], (error, results) => {
+//         if (error) {
+//             throw error;
+//         } else {
+//             res.render('editarIngredientes', { results: results[0] });
 //         }
-//         const plato_id = result.insertId;
-//         ingredientes.forEach(ingrediente => {
-//             const { ingrediente_id, cantidad, unidad, prioridad } = ingrediente;
-//             const insertPlatoIngredienteQuery = `INSERT INTO plato_ingredientes (Plato_ID, Ingrediente_ID, Cantidad, Unidad, Prioridad) VALUES (${plato_id}, ${ingrediente_id}, ${cantidad}, '${unidad}', ${prioridad})`;
-//             connection.query(insertPlatoIngredienteQuery, (err, result) => {
-//                 if (err) {
-//                     console.log('Error al insertar ingrediente en plato:', err);
-//                 }
-//             });
-//         });
-//         res.status(200).send('Plato registrado exitosamente.');
-
 //     })
 // })
 
 
-    module.exports = router;
+
+
++
+    //PROCESOS ////////////////////////// AAAAAAAAAAAAAAAAAAA/////////////////////////
+    //RUTA PARA ordenar
+
+    router.get('/ordenar', (req, res) => {
+        // Consulta para obtener las categorías de platos
+        conexion.query('SELECT * FROM plato', (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                conexion.query('SELECT * FROM clientes', (error, clientes) => {
+                    if (error) {
+                        throw error;
+                    } else {
+                        // console.log(results);
+                        // console.log(ingredientes);
+                        // Enviamos los resultados de ambas consultas a la vista
+                        res.render('ordenar', { results: results, clientes: clientes });
+                    }
+                });
+
+
+            }
+        });
+
+    });
+
+const crudPedido = require('./Controllers/crudPedido');
+router.post('/savePedido', crudPedido.save);
+router.get('/recomendacion/:ID', crudPedido.buscarRecomendacion);
+
+
+
+module.exports = router;
